f73a88adc66f0f034609e7b2c52a3e24c277370c
DIVIYA SRI S <97272477+divi5121@users.noreply.github.com>
2026-05-12 14:41:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+mport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/nav-simulator/'
 })
